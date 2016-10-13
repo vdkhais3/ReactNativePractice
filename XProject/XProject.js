4164b8ffe3d8947197663d358acad9479847bd89
Khai Vu <vdkhai@gmail.com>
2016-10-13 15:31:38 +0700

    2nd Init

---
+++ XProject/XProject.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+// import HelloWorld from './HelloWorld';
+// import Bananas from './Bananas';
+// import Greeting from './Greeting';
+// import Blink from './Blink';
+// import Style from './Style';
+// import { 
+//     FixedDimensionsBasics,
+//     FlexDimensionsBasics,
+//     FlexDirectionBasics,
+//     JustifyContentBasics,
+//     AlignItemsBasics
+// } from './Layout';
+// import {
+//     PizzaTranslator
+// } from './TextInput';
+// import {
+//     IScrolledDownAndWhatHappenedNextShockedMe
+// } from './ScrollView';
+// import {
+//     ListViewBasics
+// } from './ListView';
+
+import SimpleNavigation from './SimpleNavigation';
+
+export default class XProject extends Component {
+  	render() {
+	  	return (
+		    <SimpleNavigation/>
+  		);
+	}
+}
--- XProject/index.android.js
+++ XProject/index.android.js
@@ -1,26 +1,5 @@
-import React, { Component } from 'React';
+import React, { Component } from 'react';
 import { AppRegistry } from  'react-native';
 
-import HelloWorld from './HelloWorld';
-import Bananas from './Bananas';
-import Greeting from './Greeting';
-import Blink from './Blink';
-import Style from './Style';
-import { 
-    FixedDimensionsBasics,
-    FlexDimensionsBasics,
-    FlexDirectionBasics,
-    JustifyContentBasics,
-    AlignItemsBasics
-} from './Layout';
-import {
-    PizzaTranslator
-} from './TextInput';
-import {
-    IScrolledDownAndWhatHappenedNextShockedMe
-} from './ScrollView';
-import {
-    ListViewBasics
-} from './ListView';
-
-AppRegistry.registerComponent('XProject', () => ListViewBasics);
+import XProject from './XProject';
+AppRegistry.registerComponent('XProject', () => XProject);
