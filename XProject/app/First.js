7ac67d7047242f772359d4c427d9049138b5ab35
vdkhai <vdkhai@gmail.com>
2016-10-18 23:18:26 +0700

    5th

--- XProject/app/First.js
+++ XProject/app/First.js
@@ -6,12 +6,33 @@ import {
 	View,
 	Text,
 	TouchableHighlight,
+	TouchableWithoutFeedback,
+	DatePickerAndroid,
+	Slider,
 	StyleSheet
 } from 'react-native';
 
+var dateFormat = require('dateformat');
+//var ListPopover = require('react-native-list-popover');
 var ActionBar = require('./ActionBar');
 
 class FirstScene extends Component {
+
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			sliderValue1: 1,
+			sliderValue2: 5,
+			presetDate: new Date(2020, 4, 5),
+			allDate: new Date(2020, 4, 5),
+			simpleText: 'pick a date',
+			//minText: 'pick a date, no earlier than today',
+			//maxText: 'pick a date, no later than today',
+			//presetText: 'pick a date, preset to 2020/5/5',
+			//allText: 'pick a date between 2020/5/1 and 2020/5/10',
+		};
+	}
 
 	_goToSecondScene() {
 		this.props.navigator.push({
@@ -23,13 +44,34 @@ class FirstScene extends Component {
 		alert("Home press");
 	}
 
+	showPicker = async (stateKey, options) => {
+		try {
+			var newState = {};
+			const {action, year, month, day} = await DatePickerAndroid.open(options);
+
+			if (action === DatePickerAndroid.dismissedAction) {
+				newState[stateKey + 'Text'] = 'dismissed';
+			} else {
+				var date = new Date(year, month, day);
+				//newState[stateKey + 'Text'] = date.toLocaleDateString();
+				newState[stateKey + 'Text'] = dateFormat(date, "dddd, mmmm dS, yyyy, h:MM:ss TT");
+				newState[stateKey + 'Date'] = date;
+			}
+
+			this.setState(newState);
+		} catch ({code, message}) {
+			console.warn(`Error in example '${stateKey}': `, message);
+		}
+	 };
+
 	render() {
 		return (
 			<View>
 				<ActionBar
 					title='First scene'
 					rightTitle='Home'
-					rightPress={this._rightPress}/>
+					rightPress={this._rightPress}
+				/>
 
 				<View>
 					<Text>
@@ -41,9 +83,46 @@ class FirstScene extends Component {
 						</Text>
 					</TouchableHighlight>
 				</View>
+
+				<View>
+					<Slider
+						minimumValue={1}
+						maximumValue={31}
+						step={1}
+						{...this.props}
+						onValueChange={(value) => this.setState({sliderValue1: value})}
+					/>
+					<Text>{this.state.sliderValue1}</Text>
+				</View>
+
+				<View>
+					<Slider
+						minimumValue={1}
+						maximumValue={10}
+						step={1}
+						{...this.props}
+						onValueChange={(value) => this.setState({sliderValue2: value})}
+					/>
+					<Text>{this.state.sliderValue2}</Text>
+				</View>
+
+				<View>
+					<TouchableWithoutFeedback
+						onPress={this.showPicker.bind(this, 'simple', {date: this.state.simpleDate})}>
+						<View>
+							<Text style={styles.text}>{this.state.simpleText}</Text>
+						</View>
+					</TouchableWithoutFeedback>
+				</View>
 			</View>
 		)
 	}
 }
+
+var styles = StyleSheet.create({
+	text: {
+		color: 'black',
+	},
+});
 
 module.exports = FirstScene;
